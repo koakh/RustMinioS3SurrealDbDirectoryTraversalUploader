efa4fad51810dfb392d4b3d6a67cb2305aa673cf
Mário Monteiro <marioammonteiro@gmail.com>
2024-09-26 19:22:26 +0100

    before start to migrate surealdb package from 1.1.1 to 2.0.2, after bring production files

--- production/update-certificates.sh
+++ production/update-certificates.sh
@@ -2,7 +2,7 @@
 
 [[ EUID -ne 0 ]] && echo "This script must be run as root." && exit 1
 
-cp /etc/letsencrypt/live/koakh.com/fullchain.pem /etc/letsencrypt/live/koakh.com/privkey.pem ./certs
-
-# checki inside container certificates with
-# docker exec -it rust-sveltekit-auth-starter-nginx ls -la /etc/letsencrypt/live/koakh.com/
+cp /etc/letsencrypt/live/koakh.com/fullchain.pem /etc/letsencrypt/live/koakh.com/{fullchain.pem,privkey.pem} ./certs
+openssl x509 -in ./certs/fullchain.pem -text -noout | grep "Issuer\|Not Before\|Not After"
+# check inside container certificates with
+docker exec -it rust-sveltekit-auth-starter-nginx ls -la /etc/letsencrypt/live/koakh.com/
